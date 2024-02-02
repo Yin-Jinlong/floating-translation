c1fc23a5d1fdef048f000ce94e721649a666a561
yjl <yjl_1567@qq.com>
2024-02-02 15:11:54 +0800

    添加字典选择

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,63 @@
 <template>
-    <el-form
-            v-model="data"
-            label-width="80px"
-            style="width: calc(100% - 2em)">
-        <h3>悬浮卡片</h3>
-        <el-form-item label="背景色">
-            <el-color-picker
-                    v-model="data.cardColor"
-                    show-alpha
-                    @change="sendCard"/>
-        </el-form-item>
-        <el-form-item label="字体颜色">
-            <el-color-picker v-model="data.fontColor"
-                             show-alpha
-                             @change="sendFont"/>
-        </el-form-item>
-        <el-form-item label="显示阴影">
-            <el-switch v-model="data.showShadow" @change="sendShowShadow"/>
-        </el-form-item>
-    </el-form>
-    <div>
-        <el-button type="danger" @click="clear">重置</el-button>
-    </div>
+    <el-tabs v-model="nowPane">
+        <el-tab-pane label="卡片设置" name="card">
+            <el-form
+                    v-model="data"
+                    label-width="80px"
+                    style="width: calc(100% - 2em)">
+                <h3>悬浮卡片</h3>
+                <el-form-item label="背景色">
+                    <el-color-picker
+                            v-model="data.cardColor"
+                            show-alpha
+                            @change="sendCard"/>
+                </el-form-item>
+                <el-form-item label="字体颜色">
+                    <el-color-picker v-model="data.fontColor"
+                                     show-alpha
+                                     @change="sendFont"/>
+                </el-form-item>
+                <el-form-item label="显示阴影">
+                    <el-switch v-model="data.showShadow" @change="sendShowShadow"/>
+                </el-form-item>
+            </el-form>
+            <div>
+                <el-button type="danger" @click="clear">重置</el-button>
+            </div>
+        </el-tab-pane>
+        <el-tab-pane label="字典" name="dict">
+            <label style="font-weight: 900">当前字典：</label>
+            <el-dropdown>
+                <span class="el-dropdown-link">
+                {{ nowDict }}<el-icon class="el-icon--right"><arrow-down/></el-icon>
+                </span>
+                <template #dropdown>
+                    <el-dropdown-menu>
+                        <el-dropdown-item>default</el-dropdown-item>
+                    </el-dropdown-menu>
+                </template>
+            </el-dropdown>
+        </el-tab-pane>
+    </el-tabs>
 </template>
 
 <style lang="scss" scoped>
-
+.el-dropdown-link {
+  cursor      : pointer;
+  color       : var(--el-color-primary);
+  display     : flex;
+  align-items : center;
+}
 </style>
 
 <script lang="ts" setup>
 
-import {onMounted, reactive} from "vue";
+import {onMounted, reactive, ref} from "vue";
 import {Config, DefaultConfig, Message} from "@/Message.ts";
+import {ArrowDown} from "@element-plus/icons-vue";
+
+const nowPane = ref<'card' | 'dict'>('card')
+const nowDict = ref<string>('default')
 
 let data = reactive<Config>({
     cardColor: DefaultConfig.cardColor,
